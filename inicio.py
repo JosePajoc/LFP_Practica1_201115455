c2af945b24c5385cf2cf7aa6f1d902d1a0b34088
José Pajoc <computacion.programacion.web@gmail.com>
2021-08-02 19:22:17 -0600

    Organizar información

--- inicio.py
+++ inicio.py
@@ -6,22 +6,38 @@ opcion = 0
 ruta = ""
 archivoCargado = False
 archivoLFP = []
+curso = ""
+parametros = ""
+alumnos = []
 
 def separador():
     print("---------------------------------------------------------------------------")
 
 def cargarArchivo():
-    global ruta, archivoCargado, archivoLFP                         #Llamando variables globales
+    global ruta, archivoCargado, archivoLFP              #Llamando variables globales
     ruta = filedialog.askopenfilename(title="Seleccionar archivo flp")
     if ruta!="":
         print("La ruta es: " + ruta)
+        archivoLeido = open(ruta, "r")                    #extraer el contenido del archivo con saltos de línea
+        archivoLFP = archivoLeido.readlines()             #separar en lista cada salto de línea
+        archivoLeido.close()
         archivoCargado = True
-        archivoLeido = open(ruta, "r")                              #extraer el contenido de archivo con saltos de línea
-        archivoLFP = archivoLeido.readlines()                       #separar en lista cada salto de línea
-        print(archivoLFP)
+        procesarArchivo()
     else:
         print("No se cargo el archivo")
         archivoCargado = False
+
+def procesarArchivo():
+    global archivoLFP, curso, parametros, alumnos
+    curso = archivoLFP[0].replace("{", "")
+    print("Curso: " + curso)
+    parametros = archivoLFP[len(archivoLFP)-1].replace("}","")
+    print("Paramétros: " + parametros)
+    print("Estudiantes: ")
+    for x in range(1,len(archivoLFP)-2):
+        alumnos.append(archivoLFP[x]) 
+    print(alumnos)
+
 
 while opcion!=4:
     separador()
