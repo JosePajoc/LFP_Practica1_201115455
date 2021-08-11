8f3b47a480391b4fafc12333c9affb3de341eac5
José Pajoc <computacion.programacion.web@gmail.com>
2021-08-10 18:24:41 -0600

    Reporte HTML finalizado

--- inicio.py
+++ inicio.py
@@ -14,6 +14,7 @@ curso = ""
 parametros = ""
 alumnos = []                                                    #Lista de objetos estudiantes
 listaParametros = []
+repConsola = False
 
 def separador():
     print("-------------------------------------------------------------------------------------")
@@ -85,13 +86,13 @@ def ganoPerdioPromedio():
         cantidad = cantidad + 1
     promedio = suma / cantidad
 
-
 def mostrarConsola():
-    global curso, parametros,alumnos, gano, perdio, promedio, listaParametros
+    global curso, parametros,alumnos, gano, perdio, promedio, listaParametros, repConsola
 
     ascendenteNotas()
     ganoPerdioPromedio()
-
+    repConsola = True
+    
     separador()
     print("Curso: " + curso)
     separador()
@@ -126,7 +127,102 @@ def mostrarConsola():
             separador()
             print('Total de reaprobados: ' + str(perdio))
 
+def mostrarReporteHTML():
+    global curso, parametros,alumnos, gano, perdio, promedio, listaParametros, repConsola
+    
+    if repConsola == False:
+        ascendenteNotas()
+        ganoPerdioPromedio()
+    
+    archivoCSS = open("estilos.css", "w")
+    contenidoCSS = """html {   font-size: 20px; font-family: 'Open Sans', sans-serif; } \n
+                    h1 { font-size: 60px; text-align: center; } \n
+                    p, li {   font-size: 16px;   line-height: 2;   letter-spacing: 1px; }\n
+                    html { background-color: #00539F; }
+                    body { width: 1100px; margin: 0 auto; background-color: #FF9500; padding: 0 20px 20px 20px; border: 5px solid black; }
+                    h1 { margin: 0; padding: 20px 0; color: #00539F; text-shadow: 3px 3px 1px black; }"""
+    archivoCSS.write(contenidoCSS)
+    archivoCSS.close()
 
+    archivoHTML = open("index.html", "w")
+    archivoHTML.write("\n<!Doctype html>")
+    archivoHTML.write("\n<html>")
+    archivoHTML.write("\n<head>")
+    archivoHTML.write("\n<title>Practica 1 - LFP</title>")
+    archivoHTML.write('\n<link href="estilos.css" rel="stylesheet" type="text/css">')
+    archivoHTML.write("\n</head>")
+    archivoHTML.write("\n<body>")
+    archivoHTML.write("\n<h1>Curso: " + curso + "</h1>")
+    archivoHTML.write("\n<hr>")
+
+    archivoHTML.write("\nTotal de estudiantes: " + str(len(alumnos)))
+    archivoHTML.write("\n<hr>")
+    archivoHTML.write("\nParamétros: " +  ", ".join(listaParametros))
+    archivoHTML.write("\n<hr>")
+
+    archivoHTML.write("\n<h2>Listado general de estudiantes</h2>")
+    archivoHTML.write("\n<table border = \"1\">")
+    archivoHTML.write("\n<tr>")
+    archivoHTML.write("\n\t<td>")
+    archivoHTML.write("\n\t\tEstudiante")
+    archivoHTML.write("\n\t</td>")
+    archivoHTML.write("\n\t<td>")
+    archivoHTML.write("\n\t\tNota")
+    archivoHTML.write("\n\t</td>")
+    for x in range(len(alumnos)):
+        archivoHTML.write("\n\t<tr>")
+        archivoHTML.write("\n\t\t<td>")
+        archivoHTML.write("\n\t\t" + alumnos[x].verNombre())
+        archivoHTML.write("\n\t\t</td>")
+        archivoHTML.write("\n\t\t<td>")
+        if alumnos[x].verNota()>=60:
+            archivoHTML.write("\n\t\t<font color=\"#0000ff\">" + str(alumnos[x].verNota()) + "</font>")
+        else:
+            archivoHTML.write("\n\t\t<font color=\"#ff0000\">" + str(alumnos[x].verNota()) + "</font>")
+        archivoHTML.write("\n\t\t</td>")
+        archivoHTML.write("\n\t</tr>")
+    archivoHTML.write("\n</tr>")
+    archivoHTML.write("\n</table>")
+    
+    for i in range(len(listaParametros)):
+        if listaParametros[i].upper() == "ASC":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write("\nEstudiantes en orden ascendente")
+            archivoHTML.write("\n<ul>")
+            for i in range(len(alumnos)):
+                archivoHTML.write("\n<li>" + alumnos[i].verNombre() + ", "+ str(alumnos[i].verNota()) + "</li>")
+            archivoHTML.write("\n</ul>")    
+        elif listaParametros[i].upper() == "DESC":
+            archivoHTML.write("\n<hr>")
+            tamano = len(alumnos)
+            archivoHTML.write("\nEstudiantes en orden descendente")
+            archivoHTML.write("\n<ul>")
+            for i in range(tamano-1, -1, -1):
+                archivoHTML.write("\n<li>" + alumnos[i].verNombre() + ", "+ str(alumnos[i].verNota()) + "</li>")
+            archivoHTML.write("\n</ul>")
+        elif listaParametros[i].upper() == "AVG":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write('Promedio: ' + str(promedio))
+        elif listaParametros[i].upper() == "MIN":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write("Nota mínima, alumno: " + alumnos[0].verNombre() + " Nota: " + str(alumnos[0].verNota()))
+        elif listaParametros[i].upper() == "MAX":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write("Nota máxima, alumno: " + alumnos[len(alumnos)-1].verNombre() + " Nota: " + str(alumnos[len(alumnos)-1].verNota()))
+        elif listaParametros[i].upper() == "APR":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write('Total de aprobados: ' + str(gano))
+        elif listaParametros[i].upper() == "REP":
+            archivoHTML.write("\n<hr>")
+            archivoHTML.write('Total de reaprobados: ' + str(perdio))
+            
+    archivoHTML.write("\n</body>")
+    archivoHTML.write("\n</html>")
+    archivoHTML.close()
+    print("Se ha creado con éxito el reporte HTML...")
+    webbrowser.open_new_tab("index.html")
+
+#----------------------------------------Menú principal----------------------------------------------
 while opcion!=4:
     separador()
     print("1. Cargar archivo")
@@ -140,5 +236,10 @@ while opcion!=4:
     elif opcion == 2:
         if archivoCargado:
             mostrarConsola()
+        else:
+            print("Aún no se han cargado datos al sistema...")
+    elif opcion == 3:
+        if archivoCargado:
+            mostrarReporteHTML()
         else:
             print("Aún no se han cargado datos al sistema...")
